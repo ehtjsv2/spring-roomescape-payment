4701cc6921efaddcd3f1ede89ff1748600e00881
ehtjsv2 <ehtjsv2@naver.com>
2024-07-13 17:58:06 +0900

    test: ciTest

---
+++ src/test/java/roomescape/acceptance/ciTest.java
@@ -0,0 +1,11 @@
+package roomescape.acceptance;
+
+import org.junit.jupiter.api.Test;
+
+public class ciTest {
+
+    @Test
+    void test1(){
+        throw new IllegalArgumentException("ㅇㅇ");
+    }
+}
