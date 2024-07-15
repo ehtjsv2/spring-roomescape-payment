bcced5df2742b0cf4ee91dc324e7f19b3507e407
김도선 <ehtjsv2@naver.com>
2024-07-15 10:50:12 +0900

    test: ciTest (#1)
    * test: ciTest
    
    * test: 실패 테스트 성공으로 변경
    
    * test: 실패테스트 추가
    
    * test: 성공테스트로 변경

---
+++ src/test/java/roomescape/acceptance/CiTest.java
@@ -0,0 +1,14 @@
+package roomescape.acceptance;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class CiTest {
+
+    @Test
+    void test1(){
+        assertThat(true).isTrue();
+
+    }
+}
